a1465327b73affb0fd9ee8dc1e3a9dcd90b2385d
Gabriel Rojas <gabriel.rojasa21@gmail.com>
2025-08-05 21:42:18 -0500

    fix: fix website build

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,4 +1,3 @@
-
 export const dynamic = "force-static";
 export const fetchCache = "force-cache";
 export const revalidate = 2592000000; // this page will be revalidated via a webhook
@@ -6,6 +5,7 @@ export const revalidate = 2592000000; // this page will be revalidated via a web
 import MarketsTable from "@/components/markets-table";
 
 export default async function Home() {
+  console.log({ process: process.env.API_URL });
   const response = await fetch(process.env.API_URL + "/api/markets");
   if (!response.ok) {
     return <div>Failed to fetch markets</div>;
